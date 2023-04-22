0cd2746b87f387ccfd063f0bafe57ba20ded8402
Carl0-Garcia <131512946+Carl0-Garcia@users.noreply.github.com>
2023-04-22 14:26:39 +0800

    Update program.c

--- program.c
+++ program.c
@@ -32,8 +32,32 @@ void course_enrollment()
 }
 
 // Fees module
-void fees()
-{
+// user_courses are for courses the user selected, num_courses is the amount of courses in user_courses
+void fees(char (*user_courses)[MaxCourses][10], int num_courses) {
+  double total_fees = 0.0;
+    
+    for (int i = 0; i < num_courses; i++) {
+        char* course_name = user_courses[0][i];
+        int course_index = -1;
+        for (int j = 0; j < NumberOfCourses; j++) {
+            if (strcmp(course_name, courses[j]) == 0) {
+                course_index = j;
+                break;
+            }
+        }
+        if (course_index == -1) {
+            printf("Error: Course not found\n");
+            return;
+        }
+        char* unit_type = course_details[course_index][0];
+        if (strcmp(unit_type, "Lecture") == 0) {
+            total_fees += 1500.50;
+        } else if (strcmp(unit_type, "Lab") == 0) {
+            total_fees += 2500.00;
+        }
+        printf("%s: %s\n", course_name, unit_type);
+    }
+    printf("Total fees: $%.2f\n", total_fees);
 }
 
 // Main module
